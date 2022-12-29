2bc8f2066aeab4f2328c50047ad227734996a36f
Jonathan Welker <jwelker110@users.noreply.github.com>
2022-12-29 08:33:35 -0600

    Update to Angular v15

--- src/test.ts
+++ src/test.ts
@@ -4,8 +4,6 @@ import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 import 'zone.js/testing';
 
-declare const require: any;
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
     BrowserDynamicTestingModule,
@@ -13,7 +11,3 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
